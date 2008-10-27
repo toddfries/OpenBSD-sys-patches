c524fee4f90406f8fc96e5401f27d611cd70153e
Todd T. Fries <todd@fries.net>
2008-10-27 14:59:17 -0500

    hmm, frustrating, try something slightly more interesting

--- dev/usb/umidi_quirks.c
+++ dev/usb/umidi_quirks.c
@@ -344,13 +344,17 @@ UMQ_DEF(ROLAND, ROLAND_UA700, 3) = {
  */
 #ifdef motuquirk
 #else
-UMQ_FIXED_EP_DEF(MOTU, MOTU_FLMIDI, 0, 1, 0) = {
+UMQ_FIXED_EP_DEF(MOTU, MOTU_FLMIDI, 0, 1, 1) = {
 	/* out */
 	{ 1, 2 },
-};
-UMQ_FIXED_EP_DEF(MOTU, MOTU_FLMIDI, 1, 0, 1) = {
 	/* in */
-	{ 0, 2 }
+	{ 2, 2 },
+};
+UMQ_FIXED_EP_DEF(MOTU, MOTU_FLMIDI, 1, 1, 1) = {
+	/* out */
+	{ 2, 2 },
+	/* in */
+	{ 3, 2 }
 };
 UMQ_DEF(MOTU, MOTU_FLMIDI, 0) = {
 	UMQ_FIXED_EP_REG(MOTU, MOTU_FLMIDI, 0),
