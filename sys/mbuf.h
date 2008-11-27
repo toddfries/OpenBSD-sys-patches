eb92c70ac80557bf504f4fbc958a18907e52f3c1
Todd T. Fries <todd@fries.net>
2008-11-26 20:50:03 -0600

    sync

--- sys/mbuf.h
+++ sys/mbuf.h
@@ -1,4 +1,4 @@
-/*	$OpenBSD: mbuf.h,v 1.116 2008/11/25 19:09:34 claudio Exp $	*/
+/*	$OpenBSD: mbuf.h,v 1.118 2008/11/26 17:36:23 dlg Exp $	*/
 /*	$NetBSD: mbuf.h,v 1.19 1996/02/09 18:25:14 christos Exp $	*/
 
 /*
@@ -301,9 +301,11 @@ struct mbuf {
  * from must have M_PKTHDR set, and to must be empty.
  */
 #define M_DUP_PKTHDR(to, from) do {					\
-	(to)->m_flags = (from)->m_flags & M_COPYFLAGS;			\
+	(to)->m_flags = ((to)->m_flags & (M_EXT | M_CLUSTER));		\
+	(to)->m_flags |= (from)->m_flags & M_COPYFLAGS;			\
 	M_DUP_HDR((to), (from));					\
-	(to)->m_data = (to)->m_pktdat;					\
+	if (((to)->m_flags & M_EXT) == 0)				\
+		(to)->m_data = (to)->m_pktdat;				\
 } while (/* CONSTCOND */ 0)
 
 /*
@@ -311,9 +313,11 @@ struct mbuf {
  * from must have M_PKTHDR set, and to must be empty.
  */
 #define	M_MOVE_PKTHDR(to, from) do {					\
-	(to)->m_flags = (from)->m_flags & M_COPYFLAGS;			\
+	(to)->m_flags = ((to)->m_flags & (M_EXT | M_CLUSTER));		\
+	(to)->m_flags |= (from)->m_flags & M_COPYFLAGS;			\
 	M_MOVE_HDR((to), (from));					\
-	(to)->m_data = (to)->m_pktdat;					\
+	if (((to)->m_flags & M_EXT) == 0)				\
+		(to)->m_data = (to)->m_pktdat;				\
 } while (/* CONSTCOND */ 0)
 
 /*
@@ -424,6 +428,7 @@ struct  mbuf *m_getptr(struct mbuf *, int, int *);
 int	m_leadingspace(struct mbuf *);
 int	m_trailingspace(struct mbuf *);
 void	m_clget(struct mbuf *, int, struct ifnet *, u_int);
+void	m_clsetlwm(struct ifnet *, u_int, u_int);
 int	m_cldrop(struct ifnet *, int);
 void	m_clcount(struct ifnet *, int);
 void	m_cluncount(struct mbuf *, int);
