62acac603e2b454823aedfdc463bc26bf0aa6671
Todd T. Fries <todd@fries.net>
2008-12-03 16:19:37 -0600

    use the block size function again, but different per dale

--- arch/macppc/dev/i2s.c
+++ arch/macppc/dev/i2s.c
@@ -471,9 +471,11 @@ i2s_round_blocksize(h, size)
 	void *h;
 	int size;
 {
-	if (size < NBPG)
+	if (size > NBPG)
 		size = NBPG;
-	return size & ~PGOFSET;
+	if (size < 32)
+		size = 32;
+	return size;
 }
 
 int
