4485458760e4b0bd78ae864c428708d64dfea14e
Todd T. Fries <todd@fries.net>
2009-02-20 12:45:10 -0600

    theos i2c switch code

--- dev/i2c/i2c_scan.c
+++ dev/i2c/i2c_scan.c
@@ -60,10 +60,24 @@ struct iicprobelist probe_addrs_eeprom[] = {
 	{ 0, 0 }
 };
 
+/*
+ * Addresses at which to probe for switch devices.
+ */
+struct iicprobelist probe_addrs_switch[] = {
+//	{ 0x20, 0x27 },		/* pca8574 is dangerous */
+	{ 0x38, 0x3f },		/* pca8574a */
+	{ 0x58, 0x5b },		/* pca9454c */
+	{ 0x68, 0x6b },		/* pca9454b */
+	{ 0x70, 0x73 },		/* pca9454a */
+	{ 0, 0 }
+};
+
 char 	*iic_probe_sensor(struct device *, u_int8_t);
 char	*iic_probe_eeprom(struct device *, u_int8_t);
+char	*iic_probe_switch(struct device *, u_int8_t);
 
 #define PFLAG_SENSOR	1
+#define PFLAG_SWITCH	2
 static struct {
 	struct iicprobelist *pl;
 	char	*(*probe)(struct device *, u_int8_t);
@@ -71,6 +85,7 @@ static struct {
 } probes[] = {
 	{ probe_addrs_sensor, iic_probe_sensor, PFLAG_SENSOR },
 	{ probe_addrs_eeprom, iic_probe_eeprom, 0 },
+	{ probe_addrs_switch, iic_probe_switch, PFLAG_SWITCH },
 	{ NULL, NULL }
 };
 
@@ -921,6 +936,41 @@ iic_probe_sensor(struct device *self, u_int8_t addr)
 	}
 
 	return (name);
+}
+
+char *
+iic_probe_switch(struct device *self, u_int8_t addr)
+{
+	u_int8_t reg0;
+	int i;
+
+	reg0 = iicprobe(0x00);
+
+	/* all ports muxed through?  Rather unlikely */
+	if ((reg0 & 0x0f) == 0x0f) {
+		printf("iic addr 0x%x val 0x%x\n", addr, reg0);
+		return (NULL);
+	}
+
+	for (i = 0; i < 255; i++)
+		if (iicprobe(i) != reg0) {
+			printf("iic addr 0x%x: 0x%x 0x%x@%d\n", addr, reg0,
+			    iicprobe(i), i);
+			return (NULL);
+		}
+
+	/* more matching in driver(s) */
+	if ((addr & 0x78) == 0x28)
+		return ("pca8574");
+	if ((addr & 0x78) == 0x38)
+		return ("pca8574a");
+	if ((addr & 0x7c) == 0x70)
+		return ("pca9545a");
+	if ((addr & 0x7c) == 0x68)
+		return ("pca9545b");
+	if ((addr & 0x7c) == 0x58)
+		return ("pca9545c");
+	return (NULL);
 }
 
 char *
