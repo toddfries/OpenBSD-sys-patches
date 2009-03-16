0b4aae4b4ce2c551f78d86cee39ef5c40515c322
Todd T. Fries <todd@fries.net>
2009-03-16 13:38:23 -0500

    sync

--- arch/m68k/include/intr.h
+++ arch/m68k/include/intr.h
@@ -1,4 +1,4 @@
-/*	$OpenBSD: intr.h,v 1.11 2009/03/15 20:39:53 miod Exp $	*/
+/*	$OpenBSD: intr.h,v 1.1 2009/03/15 20:40:25 miod Exp $	*/
 /*
  * Copyright (c) 2001 Wasabi Systems, Inc.
  * All rights reserved.
@@ -33,42 +33,12 @@
  * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
  * POSSIBILITY OF SUCH DAMAGE.
  */
-/*
- * Copyright (C) 2000 Steve Murphree, Jr.
- * All rights reserved.
- *
- * Redistribution and use in source and binary forms, with or without
- * modification, are permitted provided that the following conditions
- * are met:
- * 1. Redistributions of source code must retain the above copyright
- *    notice, this list of conditions and the following disclaimer.
- * 2. Redistributions in binary form must reproduce the above copyright
- *    notice, this list of conditions and the following disclaimer in the
- *    documentation and/or other materials provided with the distribution.
- * 3. The name of the author may not be used to endorse or promote products
- *    derived from this software without specific prior written permission.
- *
- * THIS SOFTWARE IS PROVIDED BY THE AUTHOR ``AS IS'' AND ANY EXPRESS OR
- * IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES
- * OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED.
- * IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR ANY DIRECT, INDIRECT,
- * INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT
- * NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
- * DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY
- * THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
- * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF
- * THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
- */
 
-#ifndef _M88K_INTR_H_
-#define _M88K_INTR_H_
+#ifndef _M68K_INTR_H_
+#define _M68K_INTR_H_
 
 #ifdef _KERNEL
 #ifndef _LOCORE
-int	getipl(void);
-int	setipl(int level);
-int	raiseipl(int level);
-int	spl0(void);
 
 /* SPL asserts */
 #ifdef DIAGNOSTIC
@@ -90,28 +60,9 @@ void splassert_check(int, const char *);
 #define	splsoftassert(wantipl)	do { /* nothing */ } while (0)
 #endif
 
-#endif /* _LOCORE */
-
-#define splsoftclock()		raiseipl(IPL_SOFTINT)
-#define splsoftnet()		raiseipl(IPL_SOFTINT)
-#define splbio()		raiseipl(IPL_BIO)
-#define splnet()		raiseipl(IPL_NET)
-#define spltty()		raiseipl(IPL_TTY)
-#define splclock()		raiseipl(IPL_CLOCK)
-#define splstatclock()		raiseipl(IPL_STATCLOCK)
-#define	splsched()		raiseipl(IPL_SCHED)
-#define splvm()			raiseipl(IPL_VM)
-#define splhigh()		setipl(IPL_HIGH)
-
-#define	spllock()		splhigh()
-
-#define splx(x)			((x) ? setipl((x)) : spl0())
-
 /*
- * Generic software interrupt support for all m88k platforms.
+ * Generic software interrupt support for all m68k platforms.
  */
-
-#ifndef _LOCORE
 
 #define	IPL_SOFT		0
 #define	IPL_SOFTCLOCK		1
@@ -142,11 +93,11 @@ struct soft_intrq {
 	struct mutex siq_mtx;
 };
 
-void	 softintr_disestablish(void *);
-void	 softintr_dispatch(int);
 void	*softintr_establish(int, void (*)(void *), void *);
-void	 softintr_init(void);
-void	 softintr_schedule(void *);
+void	softintr_disestablish(void *);
+void	softintr_init(void);
+void	softintr_dispatch(int);
+void	softintr_schedule(void *);
 
 extern int softpending;
 
@@ -158,4 +109,4 @@ extern struct soft_intrhand *softnet_intrhand;
 #endif	/* _LOCORE */
 
 #endif /* _KERNEL */
-#endif /* _M88K_INTR_H_ */
+#endif /* _M68K_INTR_H_ */
