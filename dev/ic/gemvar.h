520fbfca456d143d88ebe3bb52362d85ec1c2c0a
Todd T. Fries <todd@fries.net>
2009-10-15 13:50:50 -0500

    sync

--- dev/ic/gemvar.h
+++ dev/ic/gemvar.h
@@ -1,4 +1,4 @@
-/*	$OpenBSD: gemvar.h,v 1.22 2009/08/10 20:29:54 deraadt Exp $	*/
+/*	$OpenBSD: gemvar.h,v 1.24 2009/10/15 17:54:54 deraadt Exp $	*/
 /*	$NetBSD: gemvar.h,v 1.1 2001/09/16 00:11:43 eeh Exp $ */
 
 /*
@@ -276,7 +276,7 @@ do {									\
 
 #ifdef _KERNEL
 void	gem_attach(struct gem_softc *, const u_int8_t *);
-int	gem_activate(struct device *, enum devact);
+int	gem_activate(struct device *, int);
 int	gem_detach(struct gem_softc *);
 int	gem_intr(void *);
 int	gem_read_srom(struct gem_softc *);
@@ -289,6 +289,7 @@ int	gem_mediachange(struct ifnet *);
 void	gem_mediastatus(struct ifnet *, struct ifmediareq *);
 
 void	gem_config(struct gem_softc *);
+void	gem_unconfig(struct gem_softc *);
 void	gem_reset(struct gem_softc *);
 int	gem_intr(void *);
 #endif /* _KERNEL */
