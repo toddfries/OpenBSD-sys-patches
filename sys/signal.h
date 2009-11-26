2b53e2a5f43a90b390523a3dff8a77597cc783cd
Todd T. Fries <todd@fries.net>
2009-11-26 12:05:24 -0600

    part 4

--- sys/signal.h
+++ sys/signal.h
@@ -43,7 +43,7 @@
 #include <sys/cdefs.h>
 #include <machine/signal.h>	/* sigcontext; codes for SIGILL, SIGFPE */
 
-#define _NSIG	32		/* counting 0; could be 33 (mask is 1-32) */
+#define _NSIG	33		/* counting 0 (mask is 1-32) */
 
 #if __BSD_VISIBLE
 #define NSIG _NSIG
@@ -87,6 +87,9 @@
 #endif
 #define SIGUSR1 30	/* user defined signal 1 */
 #define SIGUSR2 31	/* user defined signal 2 */
+#if __BSD_VISIBLE
+#define SIGTHR  32	/* thread library AST */
+#endif
 
 /*
  * Language spec says we must list exactly one parameter, even though we
