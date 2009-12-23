eb6d73d3c1148303925305e7d16f9f98223950c5
Todd T. Fries <todd@fries.net>
2009-12-22 22:31:12 -0600

    sync

--- dev/cardbus/if_athn_cardbus.c
+++ dev/cardbus/if_athn_cardbus.c
@@ -1,4 +1,4 @@
-/*	$OpenBSD: if_athn_cardbus.c,v 1.2 2009/11/23 19:11:06 damien Exp $	*/
+/*	$OpenBSD: if_athn_cardbus.c,v 1.4 2009/12/22 16:11:26 sthen Exp $	*/
 
 /*-
  * Copyright (c) 2009 Damien Bergamini <damien.bergamini@free.fr>
